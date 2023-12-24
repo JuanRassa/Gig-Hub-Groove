5120451ff580e3185115bd018e846db474f4ee38
Yane Ully <yaneully@gmail.com>
2023-12-24 10:41:36 +0000

    HomePage created and the events displayed in the Trendy concerts

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <h1>Music is for everyone one if for music</h1>
+      <Link to='/allconcerts'>
+        <h3>Trendy Concerts</h3>
+      </Link>
+      <p>Check out the most trendy concerts neer you!</p>
+      <Link>
+        <h3>Independent</h3>
+      </Link>
+      <p>Want to know some new artist?! Check out our independent list</p>
+    </div>
+  );
 }
 
 export default HomePage;
--- src/pages/trendies/AllConcerts.jsx
+++ src/pages/trendies/AllConcerts.jsx
@@ -1,22 +1,48 @@
 import React from 'react';
 import axios from 'axios';
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 const API_URL =
-  'https://www.jambase.com/jb-api/v1/artists?apikey=34602fe4-774f-4365-8a0d-ca7139ae2e76&artistName=Billie Eilish';
+  'https://www.jambase.com/jb-api/v1/events?apikey=34602fe4-774f-4365-8a0d-ca7139ae2e76';
 
 function AllConcerts() {
+  const [showEvents, setShowEvents] = useState([]);
+
   useEffect(() => {
     axios
       .get(API_URL)
       .then(response => {
-        console.log(response.data);
+        if (response.data && Array.isArray(response.data.events)) {
+          console.log(response.data.events);
+          setShowEvents(response.data.events);
+        } else {
+          console.error(error);
+        }
       })
       .catch(error => {
         console.log(error);
       });
   }, []);
-  return <div>AllConcerts</div>;
+  return (
+    <div>
+      <h1>Trendy Concerts</h1>
+
+      {showEvents.map(events => {
+        return (
+          <div key={events.identifier}>
+            <img
+              src={events.image}
+              alt='event-img'
+              style={{ height: 100, width: 200 }}
+            />
+            <h3>{events.name}</h3>
+            <h4>{events.startDate}</h4>
+            <p>{events.location.name}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default AllConcerts;
