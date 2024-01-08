2050ef36897d180531f4ba6276ed4636a202a7a3
Juan Rassa <jmrassa.dev@gmail.com>
2024-01-08 21:17:58 +0000

    Navbar: Add link to the HomePage.

--- src/components/DrawerAppBar.jsx
+++ src/components/DrawerAppBar.jsx
@@ -6,24 +6,19 @@ import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import LeftDrawer from './LeftDrawer';
+import { Link } from 'react-router-dom';
 
 function DrawerAppBar() {
   return (
     <Box sx={{ flexGrow: 10 }}>
       <AppBar position='static'>
         <Toolbar>
-          <IconButton
-            size='large'
-            edge='start'
-            color='inherit'
-            aria-label='menu'
-            sx={{ mr: 2 }}
-          >
+          <IconButton size='large' edge='start' color='inherit' aria-label='menu' sx={{ mr: 2 }}>
             <MenuIcon />
             <LeftDrawer />
           </IconButton>
           <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
-            Gig Hub Groove
+            <Link to='/'>Gig Hub Groove</Link>
           </Typography>
           <Button color='inherit'>Login</Button>
         </Toolbar>
