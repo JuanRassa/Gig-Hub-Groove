e0d6d7f7ad64dbbfe582653d8b7ffa6699efc71a
Juan Rassa <jmrassa.dev@gmail.com>
2024-01-10 21:47:19 +0000

    Creation of a false-login-register flow

---
+++ src/pages/users/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import RegisterLogin from '../../components/RegisterLogin/RegisterLogin';
+
+const Login = () => {
+  return (
+    <div className='Login'>
+      <h1 className='MainTitle'>Login</h1>
+      <RegisterLogin />
+    </div>
+  );
+};
+
+export default Login;
