5fea805052010e51fdc302afbb918ce5b0b39f69
Juan Rassa <jmrassa.dev@gmail.com>
2024-01-12 11:32:00 +0000

    feat: Add validation to the action of editing events.

--- src/pages/independent/HomeIndependent.jsx
+++ src/pages/independent/HomeIndependent.jsx
@@ -1,13 +1,18 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import EventCard from '../../components/EventCard';
 import axios from 'axios';
 import './styles.css';
+import { LoginContext } from '../../context/LoginContext';
+import { Login } from '@mui/icons-material';
 
 const API_URL = 'https://gig-hub-independent.adaptable.app/events';
 
 const HomeIndependent = () => {
   const [allIndependent, setAllIndependent] = useState([]);
+  const {
+    isLoggedCtx: [isLogged],
+  } = useContext(LoginContext);
 
   const getAllIndependent = async () => {
     const response = await axios.get(API_URL);
@@ -23,7 +28,7 @@ const HomeIndependent = () => {
   return (
     <div className='HomeIndependent'>
       <h1 className='MainTitle'>Independent Events</h1>
-      <Link to='/login'>Access the edition panel</Link>
+      <Link to={isLogged ? '/' : '/login'}>Access the edition panel</Link>
       <div className='AllIndependent__container'>
         {allIndependent.map(event => {
           return <EventCard key={event.id} events={event} />;
