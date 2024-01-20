8a17c9490ccacb7aabbd38aed6c152c0d827b747
Juan Rassa <jmrassa.dev@gmail.com>
2024-01-20 13:09:56 +0000

    Pause to the Favorites development :)

--- src/pages/users/Favorites.jsx
+++ src/pages/users/Favorites.jsx
@@ -10,15 +10,13 @@ let API_KEY = import.meta.env.VITE_API_KEY;
 const Favorites = () => {
   const [wishList, setWishList] = useState([]); // stored liked events
   const [wishlistInfo, setWishlistInfo] = useState([]);
-  const loginContext = useContext(LoginContext);
-  console.log('loginContext', loginContext.isLoggedCtx);
 
   const {
-    isLoggedCtx: [isLogged, setIsLogged],
-    usernameAuthCtx: [usernameAuth, setUsernameAuth],
+    isLoggedCtx: [isLogged],
+    usernameAuthCtx: [usernameAuth],
   } = useContext(LoginContext);
-  console.log('isLogged', isLogged);
-  console.log('usernameAuth', usernameAuth);
+  // console.log('isLogged', isLogged);
+  // console.log('usernameAuth', usernameAuth);
 
   // const [isLogged, setIsLogged] = useContext(LoginContext);
   // const [usernameAuth, setUsernameAuth] = useContext(LoginContext);
@@ -45,15 +43,16 @@ const Favorites = () => {
 
   // check if the event is in the wish list or not
   const isEventInWishList = eventId => {
-    return wishList.some(event => event.id === eventId);
+    return wishList.some(id => {
+      id == eventId;
+    });
   };
 
   // fetch the user wish list
   const fetchUserWishList = async () => {
     try {
       const response = await axios.get(`https://gig-hub-independent.adaptable.app/users/?id=1`);
-      console.log(response.data[0].favorites);
-
+      setWishList(response.data[0].favorites);
       getEventsInfo(response.data[0].favorites);
       return response.data[0].favorites;
     } catch (error) {
@@ -64,7 +63,7 @@ const Favorites = () => {
   const getIndependentId = async id => {
     try {
       const response = await axios.get(`https://gig-hub-independent.adaptable.app/events/?id=${id}`);
-      console.log(response.data[0]);
+      // console.log(response.data[0]);
       setWishlistInfo(prev => [...prev, response.data[0]]);
     } catch (error) {
       console.log(error);
@@ -74,7 +73,7 @@ const Favorites = () => {
     try {
       // const response = await axios.get(`https://www.jambase.com/jb-api/v1/events/id/jambase:11133542?apikey=${API_KEY}`)
       const response = await axios.get(`https://www.jambase.com/jb-api/v1/events/id/${identifier}?apikey=${API_KEY}`);
-      console.log(response);
+      // console.log(response);
       setWishlistInfo(prev => [...prev, response.data.event]);
     } catch (error) {
       console.log(error);
@@ -94,6 +93,7 @@ const Favorites = () => {
       console.log(error);
     }
   };
+  console.log('wishList', wishList);
 
   return (
     <div>
