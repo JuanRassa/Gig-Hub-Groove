f7f3f920e2422d1bee4028050364340bf1379e1b
Yane Ully <yaneully@gmail.com>
2024-01-22 15:19:26 +0000

    Creating the backgroundeffect component for the homepage

---
+++ src/components/BackGroundEffect.jsx
@@ -0,0 +1,14 @@
+// BackgroundEffect.js
+import React from 'react';
+import './styles.css';
+
+const BackgroundEffect = () => {
+  return (
+    <div className='background-effect'>
+      <div className='head'></div>
+      <div className='tail'></div>
+    </div>
+  );
+};
+
+export default BackgroundEffect;
