86a7894ecdbd3997724eaceab2daea803d6636f5
Yane Ully <yaneully@gmail.com>
2024-01-24 20:25:14 +0000

    Styled the Create and Edit Events Form

--- src/pages/independent/FormSections/GeneralInfo.jsx
+++ src/pages/independent/FormSections/GeneralInfo.jsx
@@ -1,5 +1,7 @@
 import React, { useContext } from 'react';
 import { CreateFormContext } from '../../../context/CreateFormContext';
+import { Flex, Text, Heading, Box } from '@chakra-ui/layout';
+import { Input, Textarea } from '@chakra-ui/react';
 
 const GeneralInfo = () => {
   const {
@@ -19,78 +21,112 @@ const GeneralInfo = () => {
     },
   } = useContext(CreateFormContext);
   return (
-    <fieldset className='CreateEvent_generalInfo'>
-      <legend>General Information</legend>
-      <div className='CreateEvent_inputContainer'>
-        <label htmlFor='event_name'>
-          Event's Name:
-          <input
-            type='text'
-            name='event_name'
-            id='event_name'
-            value={event_name}
-            onChange={e => {
-              event_name_funk(e);
-            }}
-          />
-        </label>
-      </div>
-      <div className='CreateEvent_inputContainer'>
-        <label htmlFor='event_start-date'>
-          Start Date:
-          <input
-            type='date'
-            name='event_start-date'
-            id='event_start-date'
-            value={event_start_date}
-            onChange={e => {
-              event_start_date_funk(e);
-            }}
-          />
-        </label>
-        <label htmlFor='event_end-date'>
-          End Date:
-          <input
-            type='date'
-            name='event_end-date'
-            id='event_end-date'
-            value={event_end_date}
-            onChange={e => {
-              event_end_date_funk(e);
-            }}
-          />
-        </label>
-      </div>
-      <div className='CreateEvent_inputContainer'>
-        <label htmlFor='event_image'>
-          Image URL:
-          <input
-            type='url'
-            name='event_image'
-            id='event_image'
-            value={event_image_url}
-            onChange={e => {
-              event_image_url_funk(e);
-            }}
-          />
-        </label>
-      </div>
-      <div className='CreateEvent_inputContainer'>
-        <label htmlFor='event_description'>
-          Description:
-          <textarea
-            name='event_description'
-            id='event_description'
-            cols='30'
-            rows='10'
-            value={event_event_description}
-            onChange={e => {
-              event_event_description_funk(e);
-            }}
-          />
-        </label>
-      </div>
-    </fieldset>
+    <Flex flexDirection='column' align='center' mt='8'>
+      <fieldset>
+        <Heading size='xl' color='#FDF8F2' mt='2'>
+          Creat an event
+        </Heading>
+        <Text fontWeight='600' color='#FDF8F2' mt='4'>
+          General Information
+        </Text>
+        <Box>
+          <Text
+            htmlFor='event_name'
+            fontSize='14px'
+            fontWeight='600'
+            color='#FDF8F2'
+            mt='4'
+          >
+            Event's Name:
+            <Input
+              type='text'
+              name='event_name'
+              id='event_name'
+              value={event_name}
+              onChange={e => {
+                event_name_funk(e);
+              }}
+            />
+          </Text>
+        </Box>
+
+        <Box>
+          <div className='CreateEvent_inputContainer'>
+            <Text
+              htmlFor='event_start-date'
+              fontWeight='600'
+              color='#FDF8F2'
+              mt='4'
+            >
+              Start Date:
+              <Input
+                type='date'
+                name='event_start-date'
+                id='event_start-date'
+                value={event_start_date}
+                onChange={e => {
+                  event_start_date_funk(e);
+                }}
+              />
+            </Text>
+
+            <Text
+              htmlFor='event_end-date'
+              fontWeight='600'
+              color='#FDF8F2'
+              mt='4'
+            >
+              End Date:
+              <Input
+                type='date'
+                name='event_end-date'
+                id='event_end-date'
+                value={event_end_date}
+                onChange={e => {
+                  event_end_date_funk(e);
+                }}
+              />
+            </Text>
+          </div>
+        </Box>
+
+        <Box>
+          <Text htmlFor='event_image' fontWeight='600' color='#FDF8F2' mt='4'>
+            Image URL:
+            <Input
+              type='url'
+              name='event_image'
+              id='event_image'
+              value={event_image_url}
+              onChange={e => {
+                event_image_url_funk(e);
+              }}
+            />
+          </Text>
+        </Box>
+
+        <Box>
+          <Text
+            htmlFor='event_description'
+            fontWeight='600'
+            color='#FDF8F2'
+            mt='4'
+          >
+            Description:
+            <Textarea
+              name='event_description'
+              id='event_description'
+              cols='30'
+              rows='10'
+              value={event_event_description}
+              onChange={e => {
+                event_event_description_funk(e);
+              }}
+            />
+          </Text>
+        </Box>
+      </fieldset>
+    </Flex>
   );
 };
 
--- src/pages/independent/FormSections/TypeOfEvent.jsx
+++ src/pages/independent/FormSections/TypeOfEvent.jsx
@@ -1,43 +1,60 @@
 import React, { useContext } from 'react';
 import { CreateFormContext } from '../../../context/CreateFormContext';
+import { Flex, Text } from '@chakra-ui/layout';
+import { Radio, RadioGroup, Stack } from '@chakra-ui/react';
 
 const TypeOfEvent = () => {
   const {
     states: {
       event_type_ctx: [event_type_value, set_Event_type_value],
     },
-    funks: { event_type_concert_funk_ctx: event_type_concert_funk, event_type_festival_funk_ctx: event_type_festival_funk },
+    funks: {
+      event_type_concert_funk_ctx: event_type_concert_funk,
+      event_type_festival_funk_ctx: event_type_festival_funk,
+    },
   } = useContext(CreateFormContext);
   return (
-    <fieldset className='CreateEvent_typeOfEvent'>
-      <legend>Type of Event</legend>
-      <label htmlFor='event_concert'>
-        Concert
-        <input
-          type='radio'
-          name='event_type_concert'
-          id='event_concert'
-          checked={event_type_value === 'concert'}
-          value='concert'
-          onChange={e => {
-            event_type_concert_funk(e);
-          }}
-        />
-      </label>
-      <label htmlFor='event_festival'>
-        Festival
-        <input
-          type='radio'
-          name='event_type_concert'
-          id='event_festival'
-          value='festival'
-          checked={event_type_value === 'festival'}
-          onChange={e => {
-            event_type_festival_funk(e);
-          }}
-        />
-      </label>
-    </fieldset>
+    <Flex flexDirection='column' align='center' mt='8'>
+      <fieldset>
+        <Text fontWeight='600' color='#FDF8F2' mt='2'>
+          Type of Event
+        </Text>
+
+        <RadioGroup>
+          <Stack spacing={4} direction='row'>
+            <Radio
+              type='radio'
+              name='event_type_concert'
+              id='event_concert'
+              checked={event_type_value === 'concert'}
+              value='concert'
+              onChange={e => {
+                event_type_concert_funk(e);
+              }}
+            >
+              <Text htmlFor='event_concert' fontWeight='600' color='#FDF8F2'>
+                Concert
+              </Text>
+            </Radio>
+
+            <Radio
+              type='radio'
+              name='event_type_concert'
+              id='event_festival'
+              value='festival'
+              checked={event_type_value === 'festival'}
+              onChange={e => {
+                event_type_festival_funk(e);
+              }}
+            >
+              <Text htmlFor='event_festival' fontWeight='600' color='#FDF8F2'>
+                Festival{' '}
+              </Text>
+            </Radio>
+          </Stack>
+        </RadioGroup>
+      </fieldset>
+    </Flex>
   );
 };
 
